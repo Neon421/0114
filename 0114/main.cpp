4774b0375be4f55b550976e367b71b1d469da066
Nikita Sulima <nikvip5000@gmail.com>
2022-01-14 09:40:50 +0700

    Структуры

--- 0114/main.cpp
+++ 0114/main.cpp
@@ -1,8 +1,28 @@
 #include <QCoreApplication>
+#include <iostream>
+
+struct date
+{
+    int day;
+    int mouth;
+    int year;
+};
+
+void print(date d){
+    std::cout << d.day << "." << d.mouth
+              << "." << d.year << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    date today;
+    today.day = 14;
+    today.mouth = 1;
+    today.year = 2022;
+
+    print(today);
 
     return a.exec();
 }
